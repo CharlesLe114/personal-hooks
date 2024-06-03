d1c6cfe4e8a6584c0ef373ca679819e58363ec1a
BTLe <baotoan.forwork@gmail.com>
2024-06-03 09:29:07 +0000

    hook test

--- python38_kernel_fixer/main.py
+++ python38_kernel_fixer/main.py
@@ -3,6 +3,10 @@ import argparse
 from pathlib import Path
 import json
 import os
+import sys
+
+PASS = 0
+FAIL = 1
 
 def print_arguments(arguments: list[str]):
     for argument in arguments:
@@ -16,7 +20,7 @@ def kernelspec_extraction(content):
 
 def python38_kernel_fixer(file_path):
     original_content = get_file_content(file_path)
-    # original_content["metadata"]["kernelspec"]["name"] = "python38"
+    original_content["metadata"]["kernelspec"]["name"] = "python38"
     with open(file_path, "w") as json_file:
         json.dump(original_content, json_file, indent=2, ensure_ascii=False)
 
@@ -26,11 +30,20 @@ def main():
     args = parser.parse_args()
 
     print_arguments(args.filepaths)
+
+    exit_code = PASS
+
     for filepath in args.filepaths:
-        if not filepath.endswith(".ipynb"): continue
-        kernelspec = kernelspec_extraction(get_file_content(filepath))
-        if kernelspec == "python38": continue
-        python38_kernel_fixer(filepath)
+        try:
+            if not filepath.endswith(".ipynb"): continue
+            kernelspec = kernelspec_extraction(get_file_content(filepath))
+            if kernelspec == "python38": continue
+            python38_kernel_fixer(filepath)
+        except e:
+            print(e)
+            exit_code |= FAIL
+
+    return exit_code
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
