d679a8c1faf46d4d7af93fb255815e44e0b0fd23
BTLe <baotoan.forwork@gmail.com>
2024-06-04 03:04:45 +0000

    update hook logic

--- build/lib/python38_kernel_fixer/main.py
+++ build/lib/python38_kernel_fixer/main.py
@@ -3,6 +3,7 @@ import argparse
 from pathlib import Path
 import json
 import os
+import sys
 
 def print_arguments(arguments: list[str]):
     for argument in arguments:
@@ -16,21 +17,32 @@ def kernelspec_extraction(content):
 
 def python38_kernel_fixer(file_path):
     original_content = get_file_content(file_path)
-    # original_content["metadata"]["kernelspec"]["name"] = "python38"
-    with open(file_path, "w") as json_file:
-        json.dump(original_content, json_file, indent=2, ensure_ascii=False)
+    new_content = get_file_content(file_path)
+    new_content["metadata"]["kernelspec"]["name"] = "python38"
+    if original_content != new_content:
+        with open(file_path, "w") as json_file:
+            json.dump(new_content, json_file, indent=2, ensure_ascii=False)
+        return 1
+    else:
+        return 0
+    
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filepaths", nargs="*")
     args = parser.parse_args()
 
-    print_arguments(args.filepaths)
+    exit_code = 0
+
     for filepath in args.filepaths:
         if not filepath.endswith(".ipynb"): continue
-        kernelspec = kernelspec_extraction(get_file_content(filepath))
-        if kernelspec == "python38": continue
-        python38_kernel_fixer(filepath)
+        return_value = python38_kernel_fixer(filepath)
+        print(return_value)
+        if return_value != 0:
+            print(f'Fixing kernel in {filepath}')
+        exit_code |= return_value
+
+    return exit_code
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
--- python38_kernel_fixer/main.py
+++ python38_kernel_fixer/main.py
@@ -5,9 +5,6 @@ import json
 import os
 import sys
 
-PASS = 0
-FAIL = 1
-
 def print_arguments(arguments: list[str]):
     for argument in arguments:
         print(argument)
@@ -20,28 +17,30 @@ def kernelspec_extraction(content):
 
 def python38_kernel_fixer(file_path):
     original_content = get_file_content(file_path)
-    original_content["metadata"]["kernelspec"]["name"] = "python38"
-    with open(file_path, "w") as json_file:
-        json.dump(original_content, json_file, indent=2, ensure_ascii=False)
+    new_content = get_file_content(file_path)
+    new_content["metadata"]["kernelspec"]["name"] = "python38"
+    if original_content != new_content:
+        with open(file_path, "w") as json_file:
+            json.dump(new_content, json_file, indent=2, ensure_ascii=False)
+        return 1
+    else:
+        return 0
+    
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filepaths", nargs="*")
     args = parser.parse_args()
 
-    print_arguments(args.filepaths)
-
-    exit_code = PASS
+    exit_code = 0
 
     for filepath in args.filepaths:
-        try:
-            if not filepath.endswith(".ipynb"): continue
-            kernelspec = kernelspec_extraction(get_file_content(filepath))
-            if kernelspec == "python38": continue
-            python38_kernel_fixer(filepath)
-        except e:
-            print(e)
-            exit_code |= FAIL
+        if not filepath.endswith(".ipynb"): continue
+        return_value = python38_kernel_fixer(filepath)
+        print(return_value)
+        if return_value != 0:
+            print(f'Fixing kernel in {filepath}')
+        exit_code |= return_value
 
     return exit_code
 
